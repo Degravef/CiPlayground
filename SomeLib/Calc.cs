2375e60c42f3d350bd91dd5929452be18fd9eff1
Francois Degrave <f.d@g>
2025-11-01 13:08:14 +0100

    add SomeLib Calc

---
+++ SomeLib/Calc.cs
@@ -0,0 +1,19 @@
+﻿namespace SomeLib;
+
+public class Calc
+{
+    public static int CheckedAdd(int a, int b)
+    {
+        checked
+        {
+            return a + b;
+        }
+    }
+
+    public static int SaturatingAdd(int a, int b)
+    {
+        if (b > 0 && a > int.MaxValue - b)  return int.MaxValue;
+        if (b < 0 && a < int.MinValue - b)  return int.MinValue;
+        return a + b;
+    }
+}
